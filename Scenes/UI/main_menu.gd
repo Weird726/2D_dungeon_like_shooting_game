c913e98090e4ac8550a580e909023a9f004c633e
Weird726 <1293503189@qq.com>
2026-06-06 11:35:04 +0800

    移动按钮
    Move Button

--- Scenes/UI/main_menu.gd
+++ Scenes/UI/main_menu.gd
@@ -1,3 +1,59 @@
 extends Control
-#为UI创建一个类名用于背景功能调用
+#为UI创建一个类名用于背景功能调用各个控件
 class_name MainMenu
+
+#引入节点，目的控制按钮的动画效果引入参数
+@onready var main_buttons: Control = $MainButtons
+@onready var settings_buttons: Control = $SettingsButtons
+
+#开始按钮的信号链接
+func _on_play_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+#设置按钮的信号链接
+func _on_settings_button_pressed() -> void:
+	#创建一个渐变类，用于出发设置按钮的渐变效果
+	var tween := create_tween()
+	#设置渐变属性，在其间输入信号位置，并对其全局位置属性进行位置数据的输入
+	#用于实现设置按钮被点击后进行Y值竖向进行的动画效果实现
+	tween.tween_property(main_buttons,"global_position:y",350,0.2)
+	#在渐变库中调用渐变时间间隔来实现延迟效果
+	tween.tween_interval(0.1)
+	#设置渐变属性，在其间输入信号位置，并对其全局位置属性进行位置数据的输入
+	#用于实现设置按钮被点击后进行X值横向移动动画效果的实现
+	tween.tween_property(settings_buttons,"global_position:x",145,0.3)
+
+
+#退出按钮的信号链接
+func _on_quit_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+#音乐按钮的信号链接
+func _on_music_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+#音效按钮的信号链接
+func _on_sfx_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+#窗口按钮的信号链接
+func _on_window_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+#返回按钮的信号链接
+func _on_back_button_pressed() -> void:
+	#创建一个渐变类，用于出发设置按钮的渐变效果
+	var tween := create_tween()
+	#设置渐变属性，在其间输入信号位置，并对其全局位置属性进行位置数据的输入
+	#用于实现设置按钮被点击后进行X值横向移动动画效果的实现
+	tween.tween_property(settings_buttons,"global_position:x",558,0.3)
+	#在渐变库中调用渐变时间间隔来实现延迟效果
+	tween.tween_interval(0.1)
+	#设置渐变属性，在其间输入信号位置，并对其全局位置属性进行位置数据的输入
+	#用于实现设置按钮被点击后进行Y值竖向进行的动画效果实现
+	tween.tween_property(main_buttons,"global_position:y",115,0.2)
